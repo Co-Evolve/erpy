4dc209eb83a1160843db54ee195ec2fc9a21350c
dmarzoug <dries.marzougui@gmail.com>
2023-01-02 18:16:38 +0100

    tmp safe

--- erpy/evaluators/evaluation_callbacks/video.py
+++ erpy/evaluators/evaluation_callbacks/video.py
@@ -57,19 +57,18 @@ class VideoCallback(EvaluationCallback):
         self._step_index = 0
 
 
-class ImageCompositionCallback(EvaluationCallback):
+class FrameSaverCallback(EvaluationCallback):
     def __init__(self, config: EAConfig):
-        super().__init__(config, name="ImageCompositionCallback")
+        super().__init__(config, name="FrameSaverCallback")
 
         self._frames = []
         self._env = None
         self._genome_id = None
         self._episode_index = 0
 
-        self._base_path = Path(self._ea_config.saver_config.analysis_path) / "image_composition_frames"
+        self._base_path = Path(self._ea_config.saver_config.analysis_path) / "frames"
         self._base_path.mkdir(parents=True, exist_ok=True)
 
-        self._dt = 0.5
         self._step_index = 0
         self._keep_every_nth = None
 
@@ -80,16 +79,11 @@ class ImageCompositionCallback(EvaluationCallback):
         self._genome_id = genome.genome_id
 
     def before_step(self, observations, actions) -> None:
-        if self._keep_every_nth is None:
-            self._keep_every_nth = int(self._dt / self.config.environment_config.control_timestep)
-
-        if self._step_index % self._keep_every_nth == 0:
-            self._frames.append(self._env.render())
-        self._step_index += 1
+        self._frames.append(self._env.render())
 
     def after_episode(self) -> None:
         for i, frame in enumerate(self._frames):
-            timestamp = i * self._dt
+            timestamp = i * self.config.environment_config.control_timestep
             path = self._base_path / f'genome_{self._genome_id}_frame_{timestamp}_s.png'
             frame = np.flip(frame, axis=2)
             image = Image.fromarray(frame, mode="RGB")
@@ -97,4 +91,3 @@ class ImageCompositionCallback(EvaluationCallback):
 
         self._episode_index += 1
         self._frames.clear()
-        self._step_index = 0
