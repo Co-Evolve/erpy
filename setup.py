fd096711ccce7df864396ee1635e472dbced6c18
Hector Peeters <hector.peeters@gmail.com>
2023-03-16 14:08:54 +0100

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+with open('README.md') as f:
+    readme = f.read()
+
+with open('LICENSE') as f:
+    license = f.read()
+
+setup(
+    name='erpy',
+    version='0.1.0',
+    description='Evolutionary Robotics Python -- Brain body co-optimisation framework',
+    long_description=readme,
+    url='https://github.com/Co-Evolve/erpy',
+    license=license,
+    packages=find_packages(exclude=('tests', 'docs'))
+)
