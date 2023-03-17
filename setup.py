33534306601cfcf4dd83e8868fe4349cec4e6025
dmarzoug <dries.marzougui@gmail.com>
2023-03-17 17:25:01 +0100

    Added dependency install

--- setup.py
+++ setup.py
@@ -6,12 +6,16 @@ with open('README.md') as f:
 with open('LICENSE') as f:
     license = f.read()
 
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
+
 setup(
     name='erpy',
     version='0.1.0',
-    description='Evolutionary Robotics Python -- Brain body co-optimisation framework',
+    description='Evolutionary Robotics Python -- Brain-body co-optimisation framework',
     long_description=readme,
     url='https://github.com/Co-Evolve/erpy',
     license=license,
-    packages=find_packages(exclude=('tests', 'docs'))
+    packages=find_packages(exclude=('tests', 'docs')),
+    install_requires=required
 )
