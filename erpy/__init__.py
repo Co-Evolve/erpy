be14d67fb8472e6385648cd6f052f113cab7bf21
dmarzoug <dries.marzougui@gmail.com>
2023-04-22 15:05:28 +0200

    random_state fix

--- erpy/__init__.py
+++ erpy/__init__.py
@@ -3,7 +3,8 @@ import numpy as np
 seed = 42
 random_state = np.random.RandomState(seed=42)
 
+
 def set_random_state(seed_value: int):
     global seed, random_state
     seed = seed_value
-    random_state = np.random.RandomState(seed=seed_value)
+    random_state.seed(seed_value)
